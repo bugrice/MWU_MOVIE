c6f568f368aeaeccba28b0cc69bcb7c6b3df82ad
bugrice <manssik0820@gmail.com>
2023-10-27 21:39:53 +0900

    231017

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import { ThemeProvider } from 'styled-components'
-// import { GlobalStyles } from './styles/global.js'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
