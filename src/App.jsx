e837b9e88675c118237890d6bfa6e63263849e8c
bugrice <manssik0820@gmail.com>
2023-11-20 15:51:55 +0900

    movie-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Movie from "./page/Movie"
 import Home from "./page/Home"
+import MovieList from "./page/MoiveList"
+import Header from "./components/Header"
+import MovieDetail from "./page/MovieDetail"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="movie" element={<Movie />} />
+        <Route path="movies/:type" element={<MovieList />} />
+        <Route path="movie/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   )
--- src/data/constant.js
+++ src/data/constant.js
@@ -1,3 +1,3 @@
-export const config =  {
-    API_KEY: import.meta.env.API_KEY || ""
+export const config = {
+    API_KEY: import.meta.env.VITE_APP_MOVIE_API_KEY || "" 
 }
